d5b2042290c6abf4bd6d1a573f4cd6a746a7f5dd
Marcelo Di Liscia <diliscia@gmail.com>
2021-02-13 14:46:54 -0500

    Update bus_trips_wheelchair_accessibility.sql
    adding comments for sql readibility

--- bus_trips_wheelchair_accessibility.sql
+++ bus_trips_wheelchair_accessibility.sql
@@ -1,12 +1,14 @@
 with trips_clean AS
   ( SELECT route_id,
            trip_id,
+          --- adding more readibility to the access code based on STM website
            CASE
                WHEN wheelchair_accessible = 0 then 'no wheelchair accessible information'
                WHEN wheelchair_accessible = 1 then 'wheelchair accessible'
                WHEN wheelchair_accessible = 2 then 'no wheelchair accessible'
            END AS wheelchair_accessible
-   FROM diliscia.trips)
+   FROM trips)
+ ---adding extra column to be able to create chart  
 SELECT '' AS trip,
        count(trip_id) AS total_trips,
        count(CASE
