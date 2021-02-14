2da6ec302e3011a0745d75a686a1290911c6062c
Marcelo Di Liscia <diliscia@gmail.com>
2021-02-13 20:58:50 -0500

    Update bus_trips_wheelchair_accessibility.sql
    cleaning the code and adding indentations

--- sql_code/bus_trips_wheelchair_accessibility.sql
+++ sql_code/bus_trips_wheelchair_accessibility.sql
@@ -1,18 +1,26 @@
 with trips_clean AS
-  ( SELECT route_id,
+  (
+    SELECT 
+           route_id,
            trip_id,
-          --- adding more readibility to the access code based on STM website
-           CASE
+               --- adding more readibility to the access code based on STM data dictionary
+               CASE
                WHEN wheelchair_accessible = 0 then 'no wheelchair accessible information'
                WHEN wheelchair_accessible = 1 then 'wheelchair accessible'
                WHEN wheelchair_accessible = 2 then 'no wheelchair accessible'
-           END AS wheelchair_accessible
-   FROM trips)
- ---adding extra column to be able to create chart  
-SELECT '' AS trip,
-       count(trip_id) AS total_trips,
-       count(CASE
-                 WHEN wheelchair_accessible = 'wheelchair accessible' then 1
-                 ELSE null
-             END) AS total_boardable_trips
-FROM trips_clean
+               END AS 
+          wheelchair_accessible
+   FROM 
+          trips
+  )
+SELECT 
+               COUNT(trip_id) AS 
+          total_trips,
+               COUNT 
+               (CASE
+                WHEN wheelchair_accessible = 'wheelchair accessible' then 1
+                ELSE null
+                END) AS 
+          total_boardable_trips
+FROM 
+          trips_clean
