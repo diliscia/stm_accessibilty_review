66446e63b259a8be93d9da33cdeb3b12868bb4a8
Marcelo Di Liscia <diliscia@gmail.com>
2021-02-13 20:56:29 -0500

    Update bus_stop_wheelchair_boarding.sql
    cleaning the code and adding indentations

--- sql_code/bus_stop_wheelchair_boarding.sql
+++ sql_code/bus_stop_wheelchair_boarding.sql
@@ -1,26 +1,28 @@
- with stops_clean as 
+with stops_clean as 
  (
- SELECT stop_name,
- split_part (stop_name, ' -', 1) as metro_station_name,
-    stop_code,
-    stop_id,
-            stop_lat,
-            stop_lon,
-            location_type,
-            parent_station,
-                      CASE
+ SELECT 
+         stop_name,
+         stop_code,
+         stop_id,
+              CASE
               WHEN wheelchair_boarding = 0 then 'no wheelchair boarding information'
               WHEN wheelchair_boarding = 1 then 'wheelchair boarding'
               WHEN wheelchair_boarding = 2 then 'no wheelchair boarding'
-          END AS wheelchair_boarding
-  FROM diliscia.stops
+              END AS 
+         wheelchair_boarding
+  FROM  
+         stops
   WHERE 
-  stop_name != upper(stop_name) 
-  and  stop_code > 20000
+         stop_name != upper(stop_name) ---to remove duplicated stops
+  AND  
+         stop_code > 20000
 )
 
-select  
-'' as bus,
-count(stop_id) as total_stops, 
-count( case when wheelchair_boarding = 'wheelchair boarding' then 1  else null end) as total_boardable_stops
-from stops_clean
+SELECT  
+              COUNT (stop_id) as 
+         total_stops, 
+              COUNT 
+              (CASE WHEN wheelchair_boarding = 'wheelchair boarding' then 1  else null end) as 
+         total_boardable_stops
+FROM 
+         stops_clean
