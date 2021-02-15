1f71b483328837ee68958520e46d213d1e128aaa
Marcelo Di Liscia <diliscia@gmail.com>
2021-02-14 22:07:33 -0500

    Cleaning code and rename

--- sql_code/total_bus_accessibility_average_per_route.sql
+++ sql_code/total_bus_accessibility_average_per_route.sql
@@ -5,8 +5,8 @@ with wheelchair_accessible_info_per_trip AS
           trip_id,
           wheelchair_accessible
    FROM 
-          diliscia.trips 
-          where route_id = 66
+         trips 
+
  ),
 routes_enriched_wheelchair_accessible AS
  (
@@ -19,7 +19,7 @@ routes_enriched_wheelchair_accessible AS
           route_type
    FROM 
           wheelchair_accessible_info_per_trip wai
-   LEFT JOIN  diliscia.routes rou
+   LEFT JOIN  routes rou
    ON rou.route_id = wai.route_id
  ),
 getting_stop_information AS
@@ -31,9 +31,9 @@ getting_stop_information AS
           tim.stop_sequence,
           stop.wheelchair_boarding
    FROM 
-          diliscia.stops stop
+          stops stop
    JOIN 
-          diliscia.stop_times tim
+          stop_times tim
    ON 
           tim.stop_id = stop.stop_code 
  ),
@@ -79,7 +79,6 @@ compute_full_boarding_accesibility AS
           route_long_name,
           stop_id,
           stop_name,
-
           count(case when wheelchair_accessible = 1 then 1 end ) as accesible_vehicule, 
           count(case when wheelchair_accessible = 2 then 1 end ) as innaccesible_vehicule, 
           count(case when wheelchair_boarding = 1 then 1 end ) as accesible_stop, 
@@ -93,27 +92,35 @@ compute_full_boarding_accesibility AS
           stop_id,
           stop_name
 
- )
--- , 
--- innacessible_vehicule_percentage_per_stop as 
--- (
-   SELECT  *, 
-         case when ROUND(innaccesible_vehicule *100.00)/ ROUND(NULLIF((accesible_vehicule + innaccesible_vehicule) ,0 ), 1) is null 
-             then '100' 
-             else ROUND(innaccesible_vehicule *100.00)/ ROUND(NULLIF((accesible_vehicule + innaccesible_vehicule),0 ), 1) end 
-             as innacessible_vehicule_percentage,
-         case when ROUND(innaccesible_stop *100.00)/ ROUND(NULLIF((accesible_stop + innaccesible_stop),0 ), 1) is null 
-             then '100' 
-             else ROUND(innaccesible_stop *100.00)/ ROUND(NULLIF((accesible_stop + innaccesible_stop),0 ), 1) end 
-             as innacesible_stop_percentage
-FROM compute_full_boarding_accesibility
-
-
-)
-SELECT route_short_name,
+ ), 
+acessible_vehicule_percentage_per_stop as 
+ (
+   SELECT  
+          route_id,
+          route_short_name,
           route_long_name,
-AVG(innacessible_vehicule_percentage) as average_innaccesible_vehicles,
-  AVG(innacesible_stop_percentage) as average_innaccesible_stops
-from innacessible_vehicule_percentage_per_stop
-group by  route_short_name,
+          stop_id,
+          stop_name, 
+         CASE WHEN ROUND(accesible_vehicule *100.00)/ ROUND(NULLIF((accesible_vehicule + innaccesible_vehicule) ,0 ), 1) is null 
+              THEN '100' 
+              ELSE ROUND(accesible_vehicule *100.00)/ ROUND(NULLIF((accesible_vehicule + innaccesible_vehicule),0 ), 1) end 
+              AS acessible_vehicule_percentage,
+         CASE WHEN ROUND(accesible_stop *100.00)/ ROUND(NULLIF((accesible_stop + innaccesible_stop),0 ), 1) is null 
+              THEN '100' 
+              ELSE ROUND(accesible_stop *100.00)/ ROUND(NULLIF((accesible_stop + innaccesible_stop),0 ), 1) end 
+              AS acesible_stop_percentage
+   FROM 
+         compute_full_boarding_accesibility
+)
+   SELECT 
+          route_short_name,
+          route_long_name,
+                AVG(acessible_vehicule_percentage) as 
+          average_accesible_vehicles,
+                AVG(acesible_stop_percentage) as 
+          average_accesible_stops
+   FROM 
+          acessible_vehicule_percentage_per_stop
+   GROUP BY  
+          route_short_name,
           route_long_name
